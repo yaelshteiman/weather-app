cb1e22d2823b152fb9c333a09d7286d79da7c937
yaelshteiman <yaelshtaiman@gmail.com>
2023-12-10 14:16:19 +0200

    all the div and class names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useStatee} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=Haifa&appid=8a6415435c1402ebdaf1274cf9acc406`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="container">
+            <div className="top">
+                <div className="location">
+                    <p>Haifa</p>
+                </div>
+                <div className="temp">
+                    <h1>24°</h1>
+                </div>
+                <div className="description">
+                    <p>Clouds</p>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="feels">
+                    <p>25°</p>
+                </div>
+                <div className="humidity">
+                    <p>20%</p>
+                </div>
+                <div className="wind">
+                    12 MPH
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
